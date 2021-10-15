253e13b54deb5e263e96c98c84f420cc2909a885
Julia Bayless <julia.allistair@gmail.com>
2021-10-15 15:53:50 -0500

    DB created

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "toDoList" (
+"id" SERIAL PRIMARY KEY,
+"task" VARCHAR(300) NOT NULL,
+"completed" BOOLEAN DEFAULT FALSE,
+);
+
+INSERT INTO "koalas" ("task", "completed")
+VAlUES
+('run 4 miles', FALSE),
+('grocery store', FALSE),
+('make dinner', FALSE),
+('do yard work', FALSE),
+('make breakfast', TRUE);
